c3a30c4a7646aa4d699dfd43cfc774a84c6b69e3
vincent <vinnymummo@gmail.com>
2024-04-11 08:54:47 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Vin is the champion';
+
+
 // Write your code here!
